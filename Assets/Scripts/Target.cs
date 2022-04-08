ab7c632012998e11438e0690a5fbc39c30c91304
neoboop1 <bhadley@aoiths.org>
2022-04-08 12:44:52 -0400

    Completed 5.1

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+
+{
+    
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawn = -6;
+
+    void Start() {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10),
+        Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6); }
+
+   
+    void Update()
+    {
+        
+    }
+}
