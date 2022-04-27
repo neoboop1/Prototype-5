8293f90fa31db4e96dbe9e8448d0ccd2965ae29b
neoboop1 <bhadley@aoiths.org>
2022-04-27 11:55:15 -0400

    5.1
    finished 5.1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+ 
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI
+
+    void Start() {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0); }
+
+   IEnumerator SpawnTarget()
+    {
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
